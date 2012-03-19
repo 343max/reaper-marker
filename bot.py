4ad7648ca17acd1ac2e07eadacae9ce8bacdb77b
Max Winde <max.winde@gmail.com>
2012-03-19 15:57:03 +0100

    something working-like

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
 
 from simpleOSC import *
 import irclib
+import re
+import pprint
+import gntp.notifier
+import os.path
 
 initOSCClient(port=8000)
 
 # sendOSCMsg('/bot/newchapter')
 
+#Growl
+growl = gntp.notifier.GrowlNotifier(
+    applicationName = "Crowd Marker",
+    notifications = ["New Chapter Mark"],
+    defaultNotifications = ["New Chapter Mark"]
+)
+growl.register()
 
 # Connection information
 network = 'irc.freenode.net'
@@ -22,10 +33,38 @@ server = irc.server()
 server.connect ( network, port, nick, ircname = name )
 server.join ( channel )
 
+print "Ready to mark!"
+
 # irclib.DEBUG = True
 
+def newMarker(markerName):
+	# setting marker
+	markerFile = open('/tmp/nextMarker.txt', 'w')
+	markerFile.write(markerName)
+	markerFile.close()
+
+	sendOSCMsg('/bot/newchapter')
+
+	# grwol notification
+	growl.notify(
+		noteType = "New Chapter Mark",
+ 		title = "New Chapter",
+    	description = markerName
+	)
+
+	# nicecast title
+	nicecastFile = open(os.path.expanduser('~/Library/Application Support/Nicecast/NowPlaying.txt'), 'w')
+	text = "title: {0}".format(markerName)
+	nicecastFile.write(text)
+	nicecastFile.close()
+
+newMarker('test!')
+
 def handlePubMessage ( connection, event ):
-   print event.target() + '> ' + event.source().split ( '!' ) [ 0 ] + ': ' + event.arguments() [ 0 ]
+	matches = re.compile('^#marker (.*)').findall(event.arguments() [ 0 ])
+	if (matches != []):
+		newMarker(matches[0])
+		print event.target() + '> ' + event.source().split ( '!' ) [ 0 ] + ': ' + event.arguments() [ 0 ]
 
 irc.add_global_handler ( 'pubmsg', handlePubMessage )
 
--- reaperMarker.py
+++ reaperMarker.py
@@ -2,15 +2,22 @@
 
 import urllib;
 
+CURR_PROJ = RPR_EnumProjects(-1, None, 0)
+
 if (RPR_GetPlayState() == 5):
 	markerFile = open('/tmp/nextMarker.txt')
 	markerName = urllib.quote(markerFile.read())
-	markerName = markerName.replace('%20', ' ')
-	playPosition = RPR_GetPlayPosition()
+	markerFile.close()
 
-	RPR_ShowConsoleMsg(markerName)
-	RPR_ShowConsoleMsg(playPosition)
+	markerFile = open('/tmp/nextMarker.txt', 'w')
+	markerFile.close
 
-	index = RPR_AddProjectMarker(CURR_PROJ, 0, playPosition, playPosition, markerName, -1)
-	RPR_UpdateArrange()
+	if (markerName != ''):
+			markerName = markerName.replace('%20', ' ')
+			playPosition = RPR_GetPlayPosition()
 
+			# RPR_ShowConsoleMsg(markerName)
+			# RPR_ShowConsoleMsg(playPosition)
+
+			index = RPR_AddProjectMarker(CURR_PROJ, 0, playPosition, playPosition, markerName, -1)
+			RPR_UpdateArrange()
